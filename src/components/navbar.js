62ae9076dff9e93bc7e80639bfb6fd0e96b2b5a6
matii111 <m.chaparrodaz@uandresbello.edu>
2023-02-07 23:10:30 -0300

    Se agrega logo a navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,13 +2,20 @@ import React from "react";
 
 class Navbar extends React.Component {
     render() {
-        return(
+        return (<>
+
             <div className="Navbar">
+                <a className="Logo" href="/home" rel="noreferrer noopener">
+                    <img className="formatLogo" alt="logo"
+                        src="https://raw.githubusercontent.com/Matii111/Pagina-web-Servcon/master/logoServcon512.png" />
+                </a>
                 <ul className="NavbarList">
+
                     <a href="#placeholder1"><li>placeholder1</li></a>
                     <a href="#placeholder2"><li>placeholder2</li></a>
                 </ul>
             </div>
+        </>
         );
     }
 }
