ca59c21f5d446a093fc0229b07770ec297b9fbad
matii111 <m.chaparrodaz@uandresbello.edu>
2023-02-26 19:55:31 -0300

    Se crea pagina de contactos

---
+++ src/pages/contacto.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Footer from "../components/footer";
+import Navbar from "../components/navbar";
+
+class Contacto extends React.Component {
+    render() {
+        return (
+            <>
+                <div>
+                    <Navbar />
+                    <div className="contact-box">
+                        <div>
+                            <label className="label-box-container">
+                                CONTACTANOS
+                            </label>
+                            <hr>
+                            </hr>
+                            <body className="body-box-container">
+                                <label className="label-box">
+                                    Nombre
+                                </label>
+                                <input alt="nombre" type="text" className="input-box">
+                                </input>
+                                <label className="label-box">
+                                    E-mail
+                                </label>
+                                <input alt="email" type="email" className="input-box">
+                                </input>
+                                <label className="label-box">
+                                    Comuna
+                                </label>
+                                <input alt="comuna" type="text" className="input-box">
+                                </input>
+                                <label className="label-box">
+                                    Teléfono
+                                </label>
+                                <input alt="telefono" type="text" className="input-box">
+                                </input>
+                                <label className="label-box">
+                                    Asunto
+                                </label>
+                                <input alt="asunto" type="text" className="input-box">
+                                </input>
+                                <label className="label-box">
+                                    Mensaje
+                                </label>
+                                <textarea alt="mensaje" type="text">
+                                </textarea>
+                                <button className="button-box">
+                                    Enviar
+                                </button>
+                            </body>
+                            <div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <Footer />
+                </div>
+            </>
+        );
+    }
+}
+
+export default Contacto;
